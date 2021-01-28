faafafb1833bce118aa838e8fce7e7e6819c9a77
PawelKwidzinski <solvap83@gmail.com>
2021-01-28 16:09:17 +0100

    Initial commit

---
+++ src/test/java/pl/kwidzinski/curency/CurencyGameApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kwidzinski.curency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurencyGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
