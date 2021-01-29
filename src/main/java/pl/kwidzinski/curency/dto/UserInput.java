77001f514f06e35100d078e8393ade20355286b8
PawelKwidzinski <solvap83@gmail.com>
2021-01-29 17:31:58 +0100

    Create DTO class

---
+++ src/main/java/pl/kwidzinski/curency/dto/UserInput.java
@@ -0,0 +1,15 @@
+package pl.kwidzinski.curency.dto;
+
+
+public class UserInput {
+
+    private String input;
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+}
